0f9c1f7e345ce7fa17cab180c8b76cb2d0bd1c5b
coolsync <coolsyn@outlook.com>
2021-06-09 18:27:18 +0800

    py for

---
+++ py/day02/08-for-trangle.py
@@ -0,0 +1,27 @@
+# 5 apple
+
+# for i in range(1, 6):
+#     if i == 4:
+#         print('eat bao')
+#         break
+#     print(f'eat id: %d apple' % i)
+
+# for i in range(1, 6):
+#     if i == 4:
+#         print('you a qong, no eat')
+#         continue
+#     print(f'eat id: %d apple' % i)
+
+
+# print trangle
+n = 5
+
+for i in range(n):
+    for j in range(i+1):
+        print('*', end=' ')
+    print()
+
+for i in range(n):
+    for j in range(n):
+        print('*', end=' ')
+    print()
---
+++ py/day02/09-for-else.py
@@ -0,0 +1,43 @@
+
+# s1 = 'hello world'
+s1 = 'hello python'
+
+for i in s1:
+    if i == 'p':
+        print('contain p')
+        break
+else:
+    print('not contain p')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+""" 
+Traceback (most recent call last):
+  File "/home/dart/Documents/py/day02/for-else.py", line 4, in <module>
+    for i in range(s1):
+TypeError: 'str' object cannot be interpreted as an integer
+
+"""
