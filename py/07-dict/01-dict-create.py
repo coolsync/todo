065dcaafe5fa272be562e03c892ee5b14768ae86
coolsync <coolsyn@outlook.com>
2021-06-20 20:48:43 +0800

    dict

---
+++ py/07-dict/01-dict-create.py
@@ -0,0 +1,11 @@
+# has data dict
+d1 = {'name': 'bob', 'age': 18, 'gender': 'man'}
+# print(d1)
+
+# nil dict
+d2 = {}
+# print(type(d2)) # <class 'dict'>
+
+d3 = dict()
+print(type(d3)) # <class 'dict'>
+
---
+++ py/07-dict/02-dict-add.py
@@ -0,0 +1,6 @@
+d1 = {'name': 'bob', 'age': 18, 'gender': 'man'}
+
+d1['id'] = 1000
+
+d1['name'] = 'paul'
+print(d1)
