d6fd90d12420b3d0390f0025b9e8a05497bb966b
coolsync <coolsyn@outlook.com>
2021-07-18 20:01:04 +0800

    ajax

--- web/node/ajax/03-ajax-advanced/03-url-string-encode-decode.js
+++ web/node/ajax/03-ajax-advanced/03-url-string-encode-decode.js
@@ -1,11 +1,13 @@
 let str = '使用好编辑器很重要'
 
+// 编码
 let encode_str = encodeURI(str)
 
 console.log(encode_str)
 
 console.log('------------');
 
+// 解码
 let decode_str = decodeURI('%E4%BD%BF%E7%94%A8')
 
 console.log(decode_str)
